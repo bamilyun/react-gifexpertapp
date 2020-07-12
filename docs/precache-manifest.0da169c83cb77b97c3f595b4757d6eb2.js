12fb8fdcd11cd98c7fb264f350c7681d35b52cfd
Bairon Cuesta <bamilyun@hotmail.com>
2020-07-12 16:38:42 -0500

    Docs agregado

---
+++ docs/precache-manifest.0da169c83cb77b97c3f595b4757d6eb2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3829b4cb44b04d956911f242e5f0542",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f8bf392899843c97d04",
+    "url": "/static/css/main.a93daf28.chunk.css"
+  },
+  {
+    "revision": "027ae5944804431f3db5",
+    "url": "/static/js/2.9f575afa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9f575afa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f8bf392899843c97d04",
+    "url": "/static/js/main.37530677.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
